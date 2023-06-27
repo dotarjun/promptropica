6ef0d07638716630b1cee41c2fc3a58c27bf0fbe
Arjun <arjunsingh8112@gmail.com>
2023-06-27 11:38:51 +0530

    use Session for auth

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [Providers, setProviders] = useState(null);
 
@@ -37,7 +37,7 @@ const Nav = () => {
       {/* TODO: create Mobile Nav */}
 
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -72,7 +72,7 @@ const Nav = () => {
 
       {/* MObile Nav */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"

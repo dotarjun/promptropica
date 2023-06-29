88244bbb84de08d6e93eb1c9a21498e477b01c5f
Arjun <arjunsingh8112@gmail.com>
2023-06-29 12:28:31 +0530

    Remove logs and improve readability

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -4,23 +4,22 @@ import { connectToDatabase } from "@utils/database";
 export const POST = async (req, res) => {
     const { userId, prompt, tag } = req.json();
 
-
     try {
         await connectToDatabase();
+
         const newPrompt = new Prompt({
             creator: userId,
             prompt,
             tag,
         });
-        console.log('newPrompt', newPrompt);
+        
         await newPrompt.save();
-        console.log('saved newPrompt')
+        
         return new Response(JSON.stringify(newPrompt), {
             status: 201,
         })
 
     } catch (error) {
-        console.log('------ error ----- \n', error);
         return new Response("Failed to create a new prompt", {
             status: 500,
         })

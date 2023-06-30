c5c1828c6624c8896d64ae13aa9656dc90b0419a
Arjun <arjunsingh8112@gmail.com>
2023-06-30 21:24:14 +0530

    Handle Copy

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -10,6 +10,12 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
   const [copied, setCopied] = useState('')
 
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(''), 3000);
+  }
+
   return (
     <div className='prompt_card'>
       <div className='flex justify-center items-start gap-5'>
@@ -29,7 +35,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
         </div>
 
-        <div className='copy_btn' onClick={() => { }}>
+        <div className='copy_btn' onClick={handleCopy}>
           <Image
             src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'}
             width={12}

29ddc9c1f439de0d247ea73e9022fc227dee7edb
Arjun <arjunsingh8112@gmail.com>
2023-07-01 11:03:22 +0530

    Display profile comp

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,11 +4,43 @@ import { useState, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 
+import Profile from '@components/Profile'
 
-const Profile = () => {
+
+const MyProfile = () => {
+    const { data: session } = useSession()
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await fetch(`/api/users/${session?.user.id}/posts`)
+            const data = await res.json()
+
+            setPosts(data)
+        }
+
+        if (session?.user.id) {
+            fetchPosts()
+        }
+    }, [])
+
+    const handleEdit = () => {
+
+    }
+
+    const handleDelete = async () => {
+    }
+
     return (
-        <div>Profile</div>
-        )
+        <Profile
+            name="My"
+            description="Welcome to your profile page!"
+            data={posts}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+        />
+    )
 }
 
-export default Profile
+export default MyProfile

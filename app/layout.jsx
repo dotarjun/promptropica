d4a60d674a513988d0b07484a18197ac352388c7
Arjun <arjunsingh8112@gmail.com>
2023-07-01 13:02:17 +0530

    Add Favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,9 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href={metadata.favicon} sizes="any" />
+      </head>
       <body>
         <Provider>
           <div className="main">

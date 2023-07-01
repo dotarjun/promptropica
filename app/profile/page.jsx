61b62226198e875c704eb987632f51137a20bcf9
Arjun <arjunsingh8112@gmail.com>
2023-07-01 11:40:46 +0530

    handle edit

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -8,6 +8,7 @@ import Profile from '@components/Profile'
 
 
 const MyProfile = () => {
+    const router = useRouter()
     const { data: session } = useSession()
 
     const [posts, setPosts] = useState([])
@@ -25,11 +26,11 @@ const MyProfile = () => {
         }
     }, [])
 
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
     }
 
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
     }
 
     return (

cce3b98fcda508cb4b451763a4d88de9b2ccad40
Arjun <arjunsingh8112@gmail.com>
2023-07-01 11:56:27 +0530

    remove when confirmed

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -32,7 +32,7 @@ const MyProfile = () => {
 
     const handleDelete = async (post) => {
         const hasConfirmed = confirm('Are you sure you want to delete this prompt?');
-        if (!hasConfirmed) {
+        if (hasConfirmed) {
             try {
                 await fetch(`/api/prompt/${post._id.toString()}`, {
                     method: 'DELETE',

0d24dbd5715344cacc8b52ebb6e3b314df8a5d1e
Arjun <arjunsingh8112@gmail.com>
2023-07-18 11:18:32 +0530

    update: Change name in route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -28,7 +28,7 @@ const handler = NextAuth({
                 if (!userExists) {
                     await User.create({
                         email: profile.email,
-                        username: profile.name.replaceAll(" ", "").toLowerCase(),
+                        username: profile.name,
                         image: profile.picture,
                     });
                 }

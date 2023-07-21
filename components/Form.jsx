56b2bf5e6ec0641b6fc2e826938b130bb9590779
Arjun <arjunsingh8112@gmail.com>
2023-07-21 12:01:38 +0530

    format: prettier

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,21 +1,19 @@
-import Link from "next/link"
+import Link from "next/link";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
-        <span className="blue_gradient">
-          {type} Post
-        </span>
+        <span className="blue_gradient">{type} Post</span>
       </h1>
       <p className="desc text-left max-w-md">
-        {type} and share amazing prompts with the world, and let your imagination run wild with any AI-powered platform of your choice.
+        {type} and share amazing prompts with the world, and let your
+        imagination run wild with any AI-powered platform of your choice.
       </p>
 
       <form
         onSubmit={handleSubmit}
-        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
-      >
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">
             Your AI prompt
@@ -27,7 +25,6 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             required
             className="form_textarea"
           />
-
         </label>
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">
@@ -44,7 +41,9 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
         </label>
 
         <div className="flex-end mx-3 mb-5 gap-4">
-          <Link href="/" className="text-gray-500 hover:text-gray-700 font-semibold text-sm"          >
+          <Link
+            href="/"
+            className="text-gray-500 hover:text-gray-700 font-semibold text-sm">
             Cancel
           </Link>
           <button
@@ -54,10 +53,9 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             {submitting ? `${type}...` : type}
           </button>
         </div>
-
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default Form 
+export default Form;
